53ec1c84c5fc7f37a77317292f7d245e43152cb6
Tyghe Vallard <vallardt@gmail.com>
2016-04-06 10:54:26 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'continous_delivery',
+    version = '0.0.1',
+    packages = find_packages(),
+    author = 'Tyghe Vallard',
+    author_email = 'vallardt@gmail.com',
+    description = 'testing CI/CD',
+    license = "none",
+    keywords = '',
+)
---
+++ test_code.py
@@ -0,0 +1,5 @@
+import unittest
+import code
+
+def test_some_function():
+    assert code.some_function() == 1
